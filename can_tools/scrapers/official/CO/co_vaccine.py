d66157dd5a99233a7348a141a6e5bdd444745a12
Sean McClure <sean.mcclure17@gmail.com>
2021-05-27 15:50:12 -0400

    add code to fetch CO data

--- can_tools/scrapers/official/CO/co_vaccine.py
+++ can_tools/scrapers/official/CO/co_vaccine.py
@@ -1,9 +1,13 @@
 import us
 import pandas as pd
-import requests 
+import requests
+import json
+import io
+import re
 
 from can_tools.scrapers.official.base import CountyDashboard
 from can_tools.scrapers import variables as v
+
 
 class ColoradoVaccineDemographics(CountyDashboard):
     has_location = False
@@ -12,8 +16,36 @@ class ColoradoVaccineDemographics(CountyDashboard):
     source = "https://covid19.colorado.gov/vaccine-data-dashboard"
     source_name = ""
 
+    folder = "1r095ofG8YvNj_dMWEq4XKkfhDaF8-I0n"
+    # key taken from google network request
+    key = "AIzaSyC1qbk75NzWBvSaDh6KnsjjA9pIrP4lYIE"
+
     def fetch(self):
-        return requests.get("https://drive.google.com/file/d/1pksECDG9mkLKNlBZpT4fzFBvo9isSE_p/edit")
+        url = "https://www.googleapis.com/drive/v3/files?q='{folder}'+in+parents&key={key}"
+        header = {"Referer": "https://drive.google.com/"}
+        r = requests.get(url.format(folder=self.folder, key=self.key), headers=header)
+        return r
 
     def normalize(self, data):
-        return data.content
+        # get list of all files in the folder
+        files = json.loads(data.content.decode("utf-8"))["files"]
+
+        # extract newest file by date (search for matching date in list of files)
+        date = self._retrieve_dtm1d("US/Mountain")
+        search = "covid19_vaccine_" + date.strftime("%Y-%m-%d")
+        file = [f for f in files if re.search(search, f["name"])][0]
+
+        # fetch data from matching file and load into df
+        params = {"id": file["id"]}
+        response = requests.get("https://docs.google.com/uc", params=params)
+        df = pd.read_csv(io.BytesIO(response.content), encoding="utf8")
+
+        # wrangle df
+        df = (
+            df.query(
+                "section == 'County-level Data' and category == 'Percent of Cumulative Vaccines by Demographics'"
+            )
+            .assign(dt=date)
+            .drop(columns={"date"})
+        )
+        return df
